97a67acc166ef044e50a48d2875073bdc991051a
Chris Montes <chrismontes@about.me>
2014-05-28 11:39:58 -0700

    Removes username field from user object, that will belong to profiles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  # Getting rid of this for now, we'll use gamer tags in the Player object
+  # before_filter :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  # protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u|
-        u.permit :username, :email, :password, :password_confirmation
-      end
-    end
+  #   def configure_permitted_parameters
+  #     devise_parameter_sanitizer.for(:sign_up) do |u|
+  #       u.permit :username, :email, :password, :password_confirmation
+  #     end
+  #   end
 end
