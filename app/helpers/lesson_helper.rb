80255c67351b90fa20435a69a4f1d03d78acd7e2
Chris Montes <chrismontes@about.me>
2014-06-03 01:16:40 -0700

    Markup and helpers for lessons

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -1,2 +1,7 @@
 module LessonHelper
+
+  def lesson_is_available lesson_id, user = current_user
+    user.player.gamedata.key? lesson_id || user.status.next_available_lesson == lesson_id
+  end
+
 end
